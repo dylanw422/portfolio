f11bcb22e0da6aee05c323910651736871ffb076
dylanw422 <dylanwestdev@gmail.com>
2023-04-03 13:02:26 -0500

    resume and styling

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
               <h5 className={styles.tooltip} id={styles.githubtooltip}>Github</h5>
             </div>
             <div id={styles.buttons}>
-              <button onClick={() => window.open('/pdf/dylansResume.pdf')}>View Resume</button>
+              <button onClick={() => window.open('/pdf/myResume.pdf')}>View Resume</button>
               <button onClick={() => window.open('https://github.com/dylanw422?tab=repositories')}>Github</button>
             </div>
           </div>
@@ -148,7 +148,7 @@ export default function Home() {
             <code onClick={() => window.open('mailto:dylanwestdev@gmail.com?subject=Hello Dylan!')}>dylanwestdev@gmail.com</code>
             <div id={styles.social}>
             <Image id={styles.linkedin} className={styles.socialimages} alt='' src={linkedin} onClick={() => window.open('https://linkedin.com/in/dylanwestdev')}/>
-            <Image id={styles.github} className={styles.socialimages} alt='' src={github} onClick={() => window.open('https://github.com/dylanw422')}/>
+            <Image id={styles.githubbottom} className={styles.socialimages} alt='' src={github} onClick={() => window.open('https://github.com/dylanw422')}/>
             </div>
           </div>
         </div>
